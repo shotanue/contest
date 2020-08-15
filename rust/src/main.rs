9f7388966fce663ad6242ea5bb0472321f5bac4c
shotanue <shotanue@gmail.com>
2020-08-16 00:07:03 +0900

    proconioに対応

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,87 +1,9 @@
-// stdin
-macro_rules! stdin {
-    () => {{
-        use std::io::Read;
-        let mut s = String::new();
-        std::io::stdin().read_to_string(&mut s).unwrap();
-        s
-    }};
-}
-
-// input
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-// Test
-macro_rules! test {
-    ($($input:expr => $output:expr),* $(,)*) => {
-        #[test]
-        fn solve_test() {
-            $(
-                assert_eq!(solve($input), $output);
-            )*
-        }
-    };
-}
-
 #[allow(dead_code)]
 fn gcd(x: i64, y: i64) -> i64 {
     if y == 0 {
         return x;
     }
     gcd(y, x % y)
-}
-
-#[test]
-fn test_primes() {
-    assert_eq!(primes(1), vec![1]);
-    assert_eq!(primes(140), vec![1, 2, 2, 5, 7])
 }
 
 #[allow(dead_code)]
@@ -103,22 +25,47 @@ fn primes(num: i64) -> Vec<i64> {
     return ans;
 }
 
+#[test]
+fn test_primes() {
+    assert_eq!(primes(1), vec![1]);
+    assert_eq!(primes(140), vec![1, 2, 2, 5, 7])
+}
 
+// https://techracho.bpsinc.jp/jhonda/2019_08_05/78537
+macro_rules! test {
+    ($($input:expr => $output:expr),* $(,)*) => {
+        #[test]
+        fn solve_test() {
+            $(
+                assert_eq!(solve($input), $output);
+            )*
+        }
+    };
+}
 test! {
 }
 
+use proconio::{input, fastout};
+use proconio::source::auto::AutoSource;
+
 fn solve(src: &str) -> String {
+    let source = AutoSource::from(src);
     input! {
-        source=src,
-//        n:usize,
-//        a:[i32;n],
+       from source,
+       s:String,
     }
-    let ans = "";
+    let s: String = s;
+    let ans = s;
+
     format!("{}", ans)
 }
 
 
+#[fastout]
 fn main() {
-    println!("{}", solve(&stdin!()));
-}
+    use std::io::Read;
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
 
+    println!("{}", solve(&*s));
+}
